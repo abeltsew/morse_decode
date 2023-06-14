e38b7e316fb9b4e9587641f10fd454c22ed70142
abeltsew <35835135+abeltsew@users.noreply.github.com>
2023-06-14 16:03:52 +0300

    Create Decode Char and Word

---
+++ morse_code.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+MORSE_KEY = {
+  '.-' => 'A',
+  '-...' => 'B',
+  '-.-.' => 'C',
+  '-..' => 'D',
+  '.' => 'E',
+  '..-.' => 'F',
+  '--.' => 'G',
+  '....' => 'H',
+  '..' => 'I',
+  '.---' => 'J',
+  '-.-' => 'K',
+  '.-..' => 'L',
+  '--' => 'M',
+  '-.' => 'N',
+  '---' => 'O',
+  '.--.' => 'P',
+  '--.-' => 'Q',
+  '.-.' => 'R',
+  '...' => 'S',
+  '-' => 'T',
+  '..-' => 'U',
+  '...-' => 'V',
+  '.--' => 'W',
+  '-..-' => 'X',
+  '-.--' => 'Y',
+  '--..' => 'Z'
+}.freeze
+
+def decode_char(char)
+  MORSE_KEY[char].upcase
+end
+
+puts decode_char('.-')
+
+def decode_word(word)
+  splited = word.split
+  reponse = ''
+  splited.each do |c|
+    reponse += decode_char(c)
+  end
+  reponse.upcase
+end
+
+puts decode_word('-- -.--')
